712712c1af8ec398e125dd2da348e330bacaf87a
MATTY <MATTY@MATTY-LAPTOP>
2026-02-15 16:27:47 +0000

    small change

--- ScytheButler/Commands/CofferCommand.cs
+++ ScytheButler/Commands/CofferCommand.cs
@@ -29,7 +29,7 @@ namespace ScytheButler.Commands
 
             if (balances.Count == 0)
             {
-                embedBuilder.AddField("Bank", "No contributions yet.", inline: false);
+                embedBuilder.AddField("Bank", "No banks holding GP yet.", inline: false);
             }
             else
             {
